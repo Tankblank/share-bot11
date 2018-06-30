0e938ab315d9041abd71b45678890e4fdd667d91
Tankblank <40700065+Tankblank@users.noreply.github.com>
2018-06-30 22:29:29 +0200

    Add files via upload

---
+++ Sharing-BOT.js
@@ -0,0 +1,17 @@
+﻿const Discord = require('discord.js');
+const A7MD = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+Hello 
+Pls Join And Don't Leave
+                               [ https://discord.gg/D3zxTGM ] 
+Invite Privte For You ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+A7MD.login('NDU0MTYxODM0NDgwNzYyODgx.DhhQwg.qgQa8Ql6Ou22CImU0k_dP6loQzs');
